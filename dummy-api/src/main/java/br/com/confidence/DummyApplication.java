0e802543467261c1794ce6d293d56d7f2b2765b8
Rodrigo Zamboni <joao.zamboni@dextra-sw.com>
2019-04-04 16:38:57 -0300

    new commit

--- dummy-api/src/main/java/br/com/confidence/DummyApplication.java
+++ dummy-api/src/main/java/br/com/confidence/DummyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DummyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TESTE");
 		SpringApplication.run(DummyApplication.class, args);
 	}
 }
